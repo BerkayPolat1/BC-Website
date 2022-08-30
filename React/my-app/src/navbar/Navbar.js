07b061a3bda61ccac626bf94d7abfc5d5962bbdd
YEfeEnhos <91611406+YEfeEnhos@users.noreply.github.com>
2022-08-30 10:08:31 -0400

    Revert "Topics Dropdown Attempt"

--- React/my-app/src/navbar/Navbar.js
+++ React/my-app/src/navbar/Navbar.js
@@ -1,18 +1,23 @@
 import classes from "./Navbar.module.css";
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from "react-bootstrap/Dropdown";
 
 function Navbar() {
-    return (
-      <ul className={classes.navbar_container}>
-      <DropdownButton className={classes.navbar_element} id="dropdown-basic-button" title="Topics">
-        <div className={classes.dropdown_menu}>
-          <Dropdown.Item className={classes.dropdown_element} href="/"> Entertainment</Dropdown.Item>
-          <Dropdown.Item className={classes.dropdown_element} href="/"> Arts &amp; Science </Dropdown.Item>
-          <Dropdown.Item className={classes.dropdown_element} href="/"> Sports </Dropdown.Item>
-          <Dropdown.Item className={classes.dropdown_element} href="/"> Campus Life &amp; Students </Dropdown.Item>
-        </div>
-      </DropdownButton>
+  return (
+    <div className={classes.navbar_container}>
+      <li className={classes.navbar_element}>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic" className={classes.topics_button}>
+            Topics
+          </Dropdown.Toggle>
+          <Dropdown.Menu className={classes.dropdown_menu}>
+            <Dropdown.Item href="#/action-1" className={classes.dropdown_element}> Opinions </Dropdown.Item>
+            <Dropdown.Item href="#/action-2" className={classes.dropdown_element} > Science &amp; Technology </Dropdown.Item>
+            <Dropdown.Item href="#/action-3" className={classes.dropdown_element}> Arts &amp; Entertainment </Dropdown.Item>
+            <Dropdown.Item href="#/action-4" className={classes.dropdown_element}> Features </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </li>
+      <li className={classes.navbar_element}> Campus &amp; Student Life </li>
       <li className={classes.navbar_element}>
         <a href="/"> In the News </a>
       </li>
@@ -22,7 +27,7 @@ function Navbar() {
       <li className={classes.navbar_element}>
         <a href="/"> All Stories </a>
       </li>
-      </ul>
+    </div>
   );
 }
 
