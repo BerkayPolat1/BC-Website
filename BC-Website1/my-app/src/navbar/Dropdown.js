41d7daf5360118b53030f8eaeddd57c9b90f903a
Berkay Polat <poberkaypo85@hotmail.com>
2022-09-01 00:38:56 +0300

    File Deletions and Search Bar

--- BC-Website1/my-app/src/navbar/Dropdown.js
+++ BC-Website1/my-app/src/navbar/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { MenuItems } from './MenuItems.js';
-import './Dropdown.module.css';
+import './Dropdown.css';
 import { Link } from 'react-router-dom';
 
 function Dropdown() {
