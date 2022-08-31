a9026c05c10d15e99b493a35acd507ecaea6218a
Berkay Polat <poberkaypo85@hotmail.com>
2022-08-31 22:40:09 +0300

    Package Extension Changed

--- React/my-app/src/navbar/Dropdown.js
+++ React/my-app/src/navbar/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { MenuItems } from './MenuItems.js';
-import './Dropdown.css';
+import './Dropdown.module.css';
 import { Link } from 'react-router-dom';
 
 function Dropdown() {
